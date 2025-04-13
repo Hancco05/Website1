cf0db55d0a0752e70fa8671958bf254a39aa6522
hancco05 <s.carmonawright@gmail.com>
2025-04-13 00:24:24 -0400

    commit 7

---
+++ src/main/java/com/miweb/HomeController.java
@@ -0,0 +1,13 @@
+package com.miweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // busca el archivo index.html en /templates
+    }
+}
